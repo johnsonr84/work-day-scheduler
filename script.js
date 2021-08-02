fefcaed33a07e8d438256f7116f136b7ab746f0a
Your Name <robertcjohnson1984@gmail.com>
2021-08-02 15:01:53 -0600

    add schedule function

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var now = moment().format('LLLL');
     document.getElementById("currentDay").innerHTML = now;
 
     console.log(now);
 
-    $(".save-btn").on("click", function () {
+    $(".saveBtn").on("click", function () {
         console.log(this);
         var event = $(this).siblings(".textarea").val();
         var time = $(this).parent().attr("id");
 
         localStorage.setItem(event, time);
-})
+    })
 
-$("#9AM .textarea").val(localStorage.getItem("9AM"))
+    $("#9AM .textarea").val(localStorage.getItem("9AM"));
+    
+    var timer = setInterval(schedule, 100000)
 
+function schedule() {
+
+    var eventRow = document.querySelectorAll('.event-row');
+    console.log(eventRow)
+
+    var eventTime = moment().format('HH');
+    console.log(eventTime)
+
+    $(".event-row").each(function () {
+
+        var eventBlock = $(this).attr("id")
+        console.log(eventBlock)
+
+        if (eventBlock < eventTime) {
+            $(this).removeClass("present future")
+            $(this).addClass("past")
+        } else if (eventBlock > eventTime) {
+            $(this).removeClass("past present")
+            $(this).addClass("future")
+
+        } else {
+            $(this).removeClass("future past")
+            $(this).addClass("present")
+        }
+    })
+}
+schedule();
 })
