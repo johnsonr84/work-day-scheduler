ea664972cb4cb4dbf36562e2bf187ff1c4e02521
Your Name <robertcjohnson1984@gmail.com>
2021-08-02 13:52:52 -0600

    set items to save button

--- script.js
+++ script.js
@@ -3,4 +3,15 @@ $(document).ready(function() {
     document.getElementById("currentDay").innerHTML = now;
 
     console.log(now);
+
+    $(".save-btn").on("click", function () {
+        console.log(this);
+        var event = $(this).siblings(".textarea").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(event, time);
+})
+
+$("#9AM .textarea").val(localStorage.getItem("9AM"))
+
 })
